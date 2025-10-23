10dd2ab2016020afb1578bfcdbb9aedb6a0c8772
mochacup <104591560+pawtals@users.noreply.github.com>
2025-10-23 04:06:44 -0400

    Fully setup tailwindcss v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-10-23',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  css: ["~/assets/css/main.css"],
+  vite: {
+        plugins: [tailwindcss()],
+  }
 })
